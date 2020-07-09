c5175ce8bf1eb9faefd4c7c49a2f2ba3c4f60bd7
Walmer <walmermartinez168@gmail.com>
2020-07-09 15:24:35 -0300

    Creacion de la clase resta

---
+++ src/OperacionesBasicas/Resta.java
@@ -0,0 +1,10 @@
+package OperacionesBasicas;
+
+public class Resta {
+    public static void main(String[] args) {
+        int res1 = 2;
+        int res2 = 3;
+        int respuesta = res1 * res2;
+        System.out.println("El resultado de la resta es: " + respuesta);
+    }
+}
