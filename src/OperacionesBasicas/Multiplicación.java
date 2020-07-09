7dc954a71f6d3e727a64603e8bc88236bf12a41a
Walmer <walmermartinez168@gmail.com>
2020-07-09 15:30:39 -0300

    Se adiciono la clase multiplicacion y se soluciono la clase resta

---
+++ src/OperacionesBasicas/Multiplicación.java
@@ -0,0 +1,10 @@
+package OperacionesBasicas;
+
+public class Multiplicación {
+    public static void main(String[] args) {
+        double mul1= 2;
+        double mul2 = 4;
+        double multiplcacion = mul1 * mul2;
+        System.out.println("La multiplicacion es: " + multiplcacion);
+    }
+}
--- src/OperacionesBasicas/Resta.java
+++ src/OperacionesBasicas/Resta.java
@@ -4,7 +4,7 @@ public class Resta {
     public static void main(String[] args) {
         int res1 = 2;
         int res2 = 3;
-        int respuesta = res1 * res2;
+        int respuesta = res1 - res2;
         System.out.println("El resultado de la resta es: " + respuesta);
     }
 }
