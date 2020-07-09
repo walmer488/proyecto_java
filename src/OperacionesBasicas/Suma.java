ff8e71cabe5ee8333d5897132a542df02cc6624c
Walmer <walmermartinez168@gmail.com>
2020-07-09 15:20:31 -0300

    subiendo el proyecto java

---
+++ src/OperacionesBasicas/Suma.java
@@ -0,0 +1,10 @@
+package OperacionesBasicas;
+
+public class Suma {
+    public static void main(String[] args) {
+        int num1 = 1;
+        int num2 = 2;
+        int suma = num1 + num2;
+        System.out.println("La sumas es: " + suma);
+    }
+}
---
+++ src/proyecto_java/Proyecto_java.java
@@ -0,0 +1,9 @@
+package proyecto_java;
+
+public class Proyecto_java {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
